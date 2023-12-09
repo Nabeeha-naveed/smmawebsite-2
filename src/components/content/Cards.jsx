43a012422094b25af7db6cdaf2632e77fd5766c0
Nabeeha-naveed <nabeehanaveed325@gmail.com>
2023-12-09 14:02:06 +0300

    deleted the unnessasry files,setup done,components done:)

---
+++ src/components/content/Cards.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./main.css"
+
+export default function Cards(props) {
+  return (
+    <div className='cards' >Cards</div>
+  )
+}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./hero.css"
+
+export default function Hero() {
+  return (
+    <div className='hero'></div>
+  )
+}
